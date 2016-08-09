b2e23fe0aa7d828ab751a38ebbc964910467f526
czarifis <zarifis@cs.ucsd.edu>
2016-08-09 03:19:54 -0700

    first commit

---
+++ www/js/ctrl.js
@@ -0,0 +1,44 @@
+angular.module('starter', []).controller('myCtrl', function($scope) {
+
+  var product_names = ['Microsoft Surface Book', 'Lenovo ThinkPad W540', 'MSI GT80 Titan SLI',
+     'Gigabyte P37X', 'Apple MacBook Air',
+    'HP Pavilion x360 13', 'Dell XPS 13', 'Lenovo ThinkPad X250', 'Lenovo ThinkPad T450s',
+    'Toshiba Satellite C55DT',  'Apple MacBook (2015)',
+    'HP Spectre x360', 'Lenovo Yoga 900', 'Microsoft Surface Book', 'Dell Chromebook 13',
+    'Apple iMac', 'Asus Zen AiO Pro Z240IC',
+    'Origin Genesis (2015)', 'HP Envy Phoenix', 'MSI 24GE All-in-One',
+    'Lenovo ThinkCentre M83 Tiny in One', 'Dell OptiPlex 9020 Micro', 'Zotac Zbox Magnus EN970',
+    'Acer Revo RL85', 'Intel Compute Stick', 'Apple iMac with Retina 5K Display (2015)',
+    'Microsoft Surface Pro 4', 'Samsung Galaxy Tab S2 8.0', 'Lenovo Tab 2 A10', 'Dell Venue 10 7000',
+    'Apple iPad Pro', 'Asus ZenPad S 8.0', 'Amazon Fire', 'Lenovo Yoga Tablet 2 (8-Inch Windows)',
+    'Microsoft Surface Pro 4', 'Amazon Kindle Paperwhite (2015)', "	HP's Instant Ink Line",
+    'Canon Maxify iB4020', 'Epson SureColor P800', 'Epson WorkForce Pro WF-5690', 'Epson WorkForce ET-4550 EcoTank',
+    'Canon Color ImageClass MF820Cdn', 'OKI B412dn Monochrome Printer', 'OKI MB492 Multifunction Printer',
+    'Brother MFC-J6520DW', 'Seagate Backup Plus Desktop Drive (8TB)', 'Seagate Wireless (500GB)',
+    'Seagate Backup Plus (4TB)', 'Samsung SSD 850 EVO (2TB)', 'Intel SSD 750 Series', 'Crucial MX200',
+    'Seagate Personal Cloud (2-Bay)','QNAP TS-469L', 'D-Link AC3200 Ultra Wi-Fi Router (DIR-890L/R)',
+    'Corsair Strafe', 'Logitech G910 Orion Spark', 'Corsair Gaming K65 RGB', 'Roccat Tyon',
+    'Cougar 600M Gaming Mouse', 'Logitech MX Master', 'Logitech MX Anywhere 2', 'Dell UltraSharp U3415W',
+    'Acer H257HU', 'BenQ XL2730Z', 'Lenovo ThinkPad Stack ', 'Apple iPhone 6s', 'Google Nexus 6P',
+    'Apple iPhone, 6s Plus', 'Pebble Time', 'Marshall Kilburn', 'Sennheiser HD 598', 'Olympus Toug,  TG-4',
+    'Samsung NX1', 'Olympus PEN E-PL6', 'Canon E, S Rebel T6s', 'Roku 4', 'Nvidia Shield',
+    'Alienware 15 (with Graphics Amplifier)', 'Asus Transformer Book T100HA', 'Asus Chromebook Flip C100'
+  ];
+
+
+  var products =[];
+  for (var i = 0; i< product_names.length; i++) {
+    products.push(
+      {
+        id: i,
+        name: product_names[i],
+        user_rating: chance.floating({fixed: 2, min: 2, max: 5}),
+        units_in_stock : chance.floating({fixed: 0, min: 2, max: 700}),
+        release_year : chance.floating({fixed: 0, min: 2011, max: 2016})
+      }
+    )
+  }
+
+  $scope.p = products;
+
+});
